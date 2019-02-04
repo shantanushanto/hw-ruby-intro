09b0242f65038e57ac443f9e93adf8f7997c5fac
shantanushanto <shantanushanto@gmail.com>
2019-02-04 17:47:10 -0600

    done with homework1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,96 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    val = 0
+    for i in arr
+        val+=i
+    end
+    return val
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.length == 0
+        return 0
+    elsif arr.length == 1
+        return arr[0]
+    else
+        x = arr[0]
+        y = arr[1]
+        if x<y 
+            x,y = y,x
+        end
+        ind = 2
+        while ind < arr.length
+            i = arr[ind]
+            if i >= x
+                y = x
+                x = i
+            elsif i > y
+                y = i
+            end
+            ind+=1
+        end
+        return x+y
+    end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    dict ={}
+    for val in arr
+        if dict[val]
+            dict[val]+=1
+        else
+            dict[val]=1
+        end
+    end
+
+    for val in arr
+        f = n-val
+        if f==val
+            if dict[f]>1
+                return true
+            end
+        else
+            if dict[f]
+                return true
+            end
+        end
+    end
+    false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s=="0"
+       return true 
+    end
+    /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    
+    attr_accessor :isbn
+    attr_accessor :price
+
+    def initialize(isbn,price)
+        raise ArgumentError,
+            "Illegal argument" if isbn.empty? or price<=0
+        @isbn = isbn
+        @price = price
+    end
+
+    def price_as_string
+        sprintf("$%2.2f",@price)
+    end
 end
